180e67ded263513ca96986d120615b06ad709582
SvetaPro <144365251+SvetaPro@users.noreply.github.com>
2023-12-25 23:09:32 +0500

    Создание представления vw_goods_categories

--- Views.sql
+++ Views.sql
@@ -1,3 +1,8 @@
 CREATE VIEW vw_goods AS
 SELECT id_goods, goods, price, id_categories
 FROM goods;
+
+CREATE VIEW vw_goods_categories AS
+SELECT id_categories, categories_name
+FROM goods_categories;
+
