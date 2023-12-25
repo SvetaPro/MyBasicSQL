83f8de90a8d549e26fc14610fbc33667d980432c
SvetaPro <144365251+SvetaPro@users.noreply.github.com>
2023-12-25 23:10:17 +0500

    Создания запроса - подбор количества товаров по категориям

--- Views.sql
+++ Views.sql
@@ -6,3 +6,8 @@ CREATE VIEW vw_goods_categories AS
 SELECT id_categories, categories_name
 FROM goods_categories;
 
+SELECT c.categories_name, COUNT(id_goods)
+FROM vw_goods AS g
+LEFT JOIN vw_goods_categories AS c
+ON g.id_categories = c.id_categories
+GROUP BY c.categories_name;
